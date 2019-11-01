1658fd0b69e987ca741da352e0c84660c96ea6e5
libertymagumise <43039475+libemurg@users.noreply.github.com>
2019-11-01 12:47:15 +0200

    byte.h

---
+++ byte.h
@@ -0,0 +1,22 @@
+/*
+repreof(byte) from repre of hexa
+domain_repre                
+0: 0->0000        15: f->1111
+0:00->00000000   255:ff->11111111
+*/
+#define w_i_p work_in_progress
+#if defined(_#CODE)
+#define(_Byte)
+  #if defined(_Byte)
+    #if defined(_Hex)
+    //0x:w_i_p
+      #define _ByteMin 0x00
+      #define _ByteMax 0xff
+      #if defined(_Signed)
+      //size:w_i_p
+        #define _SByteMin 
+        #define _SByteMax
+      #endif
+    #endif
+  #endif
+#endif
